2144cdd5cf569903542d1ecbc04b602be1b8fd0c
maxmcgrath1 <tuc06096@temple.edu>
2022-02-11 13:32:41 -0500

    added citylist class and url

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from re import template
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
@@ -5,3 +6,12 @@ from django.views.generic.base import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
+
+
+class CityList(TemplateView):
+    template_name = "city_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cities"] = cities
+        return context
