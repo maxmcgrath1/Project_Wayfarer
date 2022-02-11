2af6f933fc3eac524e23ad05ba8397ac058c2de8
maxmcgrath1 <tuc06096@temple.edu>
2022-02-11 16:52:31 -0500

    Added a link to cities on home

--- db/create_cities.sql
+++ db/create_cities.sql
@@ -1,11 +1,11 @@
 INSERT INTO main_app_city(name, image, population, attractions, created_at)
    VALUES
-    ('Los Angeles', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Los_Angeles_with_Mount_Baldy.jpg/1024px-Los_Angeles_with_Mount_Baldy.jpg', 4000000, 'Hollywood Sign', 'now');
+      ('Los Angeles', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Los_Angeles_with_Mount_Baldy.jpg/1024px-Los_Angeles_with_Mount_Baldy.jpg', 4000000, 'Hollywood Sign', 'now');
 
 INSERT INTO main_app_city(name, image, population, attractions, created_at)
    VALUES
-    ('Aspen', 'https://media.cntraveler.com/photos/5fda74245344ec5351d1fca8/master/pass/AspenColorado-2020-GettyImages-956105512.jpg', 7300, 'Aspen Mountain', 'now');
+      ('Aspen', 'https://media.cntraveler.com/photos/5fda74245344ec5351d1fca8/master/pass/AspenColorado-2020-GettyImages-956105512.jpg', 7300, 'Aspen Mountain', 'now');
 
 INSERT INTO main_app_city(name, image, population, attractions, created_at)
    VALUES
-    ('Orlando', 'https://orlandoinformer.com/wp-content/uploads/2021/05/Universal-Orlando-Resort-globe-cropped.jpg', 280000, 'Walt Disney World', 'now');
+      ('Orlando', 'https://orlandoinformer.com/wp-content/uploads/2021/05/Universal-Orlando-Resort-globe-cropped.jpg', 280000, 'Walt Disney World', 'now');
