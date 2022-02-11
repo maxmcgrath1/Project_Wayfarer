ae93fb642c7e4010e80c196e489f85c0dac21dcf
maxmcgrath1 <tuc06096@temple.edu>
2022-02-11 12:55:55 -0500

    refactored home view

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
-# Create your views here.
-
-class Home(View):
-    def get(self, request):
-        return HttpResponse("This is our wayfarer home page. It's good")
+class Home(TemplateView):
+    template_name = "home.html"
