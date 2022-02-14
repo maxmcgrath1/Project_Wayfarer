51c23ef3b268c702308a0ac29c5d18fd430550ca
maxmcgrath1 <tuc06096@temple.edu>
2022-02-14 10:50:13 -0500

    added url for city detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('cities/', views.CityList.as_view(), name="city_list"),
     path('cities/new', views.CityCreate.as_view(), name="city_create"),
+    path('cities/<int:pk>/', views.CityDetail.as_view(), name ="city_detail"),
 ]
